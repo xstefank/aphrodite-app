d12a8ced99db9f5346011158d28bf0f8afd3812d
xstefank <xstefank122@gmail.com>
2017-11-16 16:27:08 +0100

    pick an example issue

--- src/main/java/org/jboss/AproditeMain.java
+++ src/main/java/org/jboss/AproditeMain.java
@@ -1,4 +1,18 @@
 package org.jboss;
 
+import org.jboss.set.aphrodite.Aphrodite;
+import org.jboss.set.aphrodite.domain.Issue;
+
+import java.net.URL;
+
 public class AproditeMain {
+
+    public static void main(String[] args) throws Exception {
+        Aphrodite aphrodite = Aphrodite.instance();
+
+        Issue issue = aphrodite.getIssue(new URL("https://issues.jboss.org/browse/JBEAP-3038"));
+        System.out.println(issue);
+
+        aphrodite.close();
+    }
 }
